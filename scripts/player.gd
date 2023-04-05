e37b3eda6b39d68e61ecec29ebc9885f1054b5ed
painttist <painttist@gmail.com>
2023-04-06 03:06:02 +0800

    Player: movement polish

--- scripts/player.gd
+++ scripts/player.gd
@@ -11,7 +11,7 @@ const TWEEN_DURATION = 0.3
 
 var tween 
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	if tween is Tween:
 		if tween.is_running():
 			return
@@ -19,10 +19,20 @@ func _physics_process(delta):
 		tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
 		tween.tween_property(self, "transform", transform.translated_local(Vector3.FORWARD * 2), TWEEN_DURATION)
 		animation.play("head_bob")
+	elif Input.is_action_pressed("forward"):
+		print_debug("touching ", ray_front.get_collider().name)
 	if Input.is_action_pressed("backward") and not ray_back.is_colliding():
 		tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
 		tween.tween_property(self, "transform", transform.translated_local(Vector3.BACK * 2), TWEEN_DURATION)
 		animation.play("head_bob")
+	if Input.is_action_pressed("left") and not ray_left.is_colliding():
+		tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+		tween.tween_property(self, "transform", transform.translated_local(Vector3.LEFT * 2), TWEEN_DURATION)
+		animation.play("head_tilt_left")
+	if Input.is_action_pressed("right") and not ray_right.is_colliding():
+		tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+		tween.tween_property(self, "transform", transform.translated_local(Vector3.RIGHT * 2), TWEEN_DURATION)
+		animation.play("head_tilt_right")
 	if Input.is_action_pressed("turn-left"):
 		tween = create_tween().set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
 		tween.tween_property(self, "transform", transform.rotated_local(Vector3.UP, PI/2), TWEEN_DURATION)
