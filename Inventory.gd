5a58dad32e10e7088df0b55cbb1840369449135f
Alan Lai <37102053+Alsctiho@users.noreply.github.com>
2023-04-06 13:48:34 +0800

    Inventory: set up

---
+++ Inventory.gd
@@ -0,0 +1,43 @@
+extends Panel
+
+class_name Inventory
+
+var inventory_slots = [
+	[null, null, null], 
+	[null, null, null]
+];
+
+enum {
+	ARROW_UP,
+	SWORD,
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var children = get_children()
+	var grid;
+	for child in children:
+		if child is GridContainer:
+			grid = child
+			break
+	
+	var slots = grid.get_children()
+	var slot_count = 0
+	for slot in slots:
+		if is_instance_of(slot, InventorySlot):
+			inventory_slots[slot_count / 3][slot_count % 3] = slot
+			slot_count += 1
+			
+#	# check inventory init
+#	print("check inventory init")
+#	for row in range(2):
+#		for col in range(3):
+#			print(inventory[row][col])
+
+func check_availble_for_place() -> bool:
+	print("check")
+	return true
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
---
+++ InventorySlot.gd
@@ -0,0 +1,23 @@
+extends TextureRect
+
+class_name InventorySlot
+
+var inventory: Inventory
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	inventory = get_parent().get_parent()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _can_drop_data(at_position, data) -> bool:
+#	print("can drop data")
+	return inventory.check_availble_for_place()
+
+func _drop_data(at_position, data):
+	print("drop data")
+	pass
